a1228180305decde865112a495c38d659f3dec7f
premot <premote@proton.me>
2023-07-11 09:38:37 +0300

    update

---
+++ xstartu.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+am start -n com.termux.x11/.MainActivity &&
+
+
+# The path of Ubuntu rootfs
+
+UBUNTUPATH="/data/local/tmp/pimount"
+
+
+export TMPDIR=$UBUNTUPATH/tmp
+export CLASSPATH=$(/system/bin/pm path com.termux.x11 | cut -d: -f2)
+/system/bin/app_process / com.termux.x11.CmdEntryPoint :0 &
+
+
+# Fix setuid issue
+
+busybox mount -o remount,dev,suid /data
+
+
+
+busybox mount --bind /dev $UBUNTUPATH/dev
+
+busybox mount --bind /sys $UBUNTUPATH/sys
+
+busybox mount --bind /proc $UBUNTUPATH/proc
+
+busybox mount -t devpts devpts $UBUNTUPATH/dev/pts
+
+
+
+# Mount sdcard
+
+#busybox mount --bind /sdcard $UBUNTUPATH/sdcard
+
+
+
+# chroot into Ubuntu
+
+busybox chroot $UBUNTUPATH /bin/su - root
+
+
+
+# Umount everything after exit
+
+#busybox umount $UBUNTUPATH/dev/pts
+
+#busybox umount $UBUNTUPATH/dev
+
+#busybox umount $UBUNTUPATH/proc
+
